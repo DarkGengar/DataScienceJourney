67d012b8657d7eaf7e791e1768d00cfea13d196c
DarkGengar <joel.nauschuetz@web.de>
2019-04-15 07:17:34 +0200

    Working on Pokémon Database

--- PkmDatabase.sql
+++ PkmDatabase.sql
@@ -2,7 +2,7 @@ USE PkmDatabase
 
 /* Pokédex Table */
 CREATE TABLE Pokedex (
-    id INT PRIMARY KEY,
+    id int NOT NULL PRIMARY KEY,
     name text,
     ability text,
     type1 text,
@@ -12,8 +12,8 @@ CREATE TABLE Pokedex (
 INSERT INTO Pokedex
 VALUES (1, 'Bulbasaur', 'Overgrow', 'Grass', 'Poison');
 
-INSERT INTO Pokedex (id, name, ability, type1)
-VALUES (7, 'Squirtle', 'Torrent', 'Water');
+INSERT INTO Pokedex
+VALUES (7, 'Squirtle', 'Torrent', 'Water', '-');
 
 INSERT INTO Pokedex
 VALUES (94, 'Gengar', 'Cursed Body', 'Ghost', 'Poison');
